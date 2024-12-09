f76480c6c925e7923d510c187fc40d969eeaeaa3
brenoob <breno_1993@outlook.com>
2024-12-08 22:48:46 -0300

    fix(eslint): adjust ESLint rules and parser configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
     'no-console': 'warn',
+    'vue/no-multiple-template-root': 'off', // Disable if necessary
+    'vue/no-v-html': 'warn', // Warn against use of v-html to prevent XSS
   },
-}).override('nuxt/vue/setup', (config) => {
-  return {
-    ...config,
-    languageOptions: {
-      ecmaVersion: 2020, // Define a versão do ECMAScript
-      sourceType: 'module',  // Permite usar imports no código
+  languageOptions: {
+    parserOptions: {
+      ecmaVersion: 2020, // Defina a versão do ECMAScript
+      sourceType: 'module', // Permita usar imports no código
+      project: './tsconfig.json', // Aponte para o tsconfig.json
+      createDefaultProgram: true, // Cria o programa TypeScript se necessário
     },
-  };
-});
+  },
+})
